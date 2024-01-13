49e4cc6f3cd765a8d88a2df3f9025e62af80c164
Philip Li 42 <macpilot23@outlook.com>
2024-01-13 19:16:39 +0000

    Refactoring

--- source/bonus.c
+++ source/bonus.c
@@ -6,7 +6,7 @@
 /*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 22:54:23 by Philip            #+#    #+#             */
-/*   Updated: 2024/01/12 23:05:21 by Philip           ###   ########.fr       */
+/*   Updated: 2024/01/13 19:15:25 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,109 +20,147 @@
  * [ ] Runs each command in a child process
  */
 
-void	err_msg(const char *s)
+/**
+ * @brief 
+ * Close all the write / read ends in an array of pipes.
+ * 
+ * @param pipes 
+ * int array storing the file descriptors of all opened pipes.
+ * @param pipe_count 
+ * The total amount of the pipes. Every one pipe has two ends and hence two
+ * elements.
+ */
+void	close_all_pipes(int	*pipes, int pipe_count)
 {
-	write(STDERR_FILENO, s, ft_strlen(s));
+	int	i;
+
+	if (!pipes)
+		return ;
+	i = 0;
+	while (i < pipe_count * 2)
+		close(pipes[i++]);
 }
 
 int	main(int argc, char **argv)
 {
-	pid_t	id;
-
 	if (argc < 5)
 	{
-		// err_msg("command incomplete\n");
+		ft_putendl_fd("command incomplete", STDERR_FILENO);
 		return (1);
 	}
 
-	/* Check if infile exists------------------------------------------------ */
+	/**
+	 * @brief Check if infile exists
+	 * ------------------------------------------------------------------------ 
+	 * @param argv[1] (infile name)
+	 * @return infile_fd, which equals to -1 when open fails
+	*/
 	int		infile_fd;
-	char	*str;
 
 	if (ft_strncmp(argv[1], "here_doc", ft_strlen("here_doc") + 1) == 0)
 	{
-		infile_fd = STDIN_FILENO;
+		ft_printf("here doc mode\n");
+		// infile_fd = STDIN_FILENO;
 	}
 	else
-	{
 		infile_fd = open(argv[1], O_RDONLY);
-	}
 	if (infile_fd == -1)
 	{
-		str = format_string("pipex: %s: ", argv[1]);
-		write(STDERR_FILENO, str, ft_strlen(str));
-		perror("");
+		perror(argv[1]);
 		return (1);
 	}
 
-	/* Check if commands exist ------------------------------------------*/
-	// $ ./pipex file1 cmd1 cmd2 cmd3 ... cmdn file2
-	// follows
-	// $ < file1 cmd1 | cmd2 | cmd3 ... | cmdn > file2
-	// number of cmds: argc - 3
-	// ./pipex infile cmd1 cmd2 cmd3 outfile
-	// 0       1      2    3    4    5
-	// argc 6, 
-	int	i;
-	int	j;
-	char	***cmds;
+	/**
+	 * @brief Check if commands exist 
+	 * =========================================================================
+	 * @param cmd_count In this case (argc - 3)
+	 * @param argv To format the executable path for commands
+	 * 
+	 * @return 
+	 * char ***cmds_argv:
+	 * {
+	 *   {"cmd1", "flag1", ...},
+	 *   {"cmd2", "flag1", ...},
+	 *   ...
+	 * }
+	 * 
+	 * char **cmds_path:
+	 * {
+	 *   {"/usr/bin/cmd1"},
+	 *   {"/usr/bin/cmd2"},
+	 *   ...
+	 * }
+	 * 
+	 */
+	int		i;
+	int		j;
+	char	***cmds_argv;
 	int		cmd_count;
 
-	// ft_printf("Checking the commands...\n");
 	/* Split commands names and flags */
 	cmd_count = argc - 3;
-	cmds = (char ***)ft_calloc(cmd_count + 1, sizeof(char **));
-	if (!cmds)
+	cmds_argv = (char ***)ft_calloc(cmd_count + 1, sizeof(char **));
+	if (!cmds_argv)
 		return (2);
 	i = 2;
 	j = 0;
 	while (i < argc - 1)
-		cmds[j++] = ft_split(argv[i++], ' ');
+		cmds_argv[j++] = ft_split(argv[i++], ' ');
 
 	/* Build command program paths */
-	char	**cmds_paths;
-	cmds_paths = (char **)ft_calloc(cmd_count + 1, sizeof(char *));
-	if (!cmds_paths)
+	char	**cmds_path;
+
+	cmds_path = (char **)ft_calloc(cmd_count + 1, sizeof(char *));
+	if (!cmds_path)
 	{
 		// free
 		return (3);
 	}
+
 	i = 0;
 	while (i < cmd_count)
 	{
-		cmds_paths[i] = format_string("/usr/bin/%s", cmds[i][0]);
+		cmds_path[i] = format_string("/usr/bin/%s", cmds_argv[i][0]);
 		i++;
 	}
-	i = 0;
-	/* while (cmds_paths[i])
-	{
-		ft_printf("cmd[%d] path: %s\n", i + 1, cmds_paths[i]);
-		i++;
-	} */
 
 	/* Checking the command programs */
 	i = 0;
 	while (i < cmd_count)
 	{
-		// ft_printf("Checking %s\n", cmds[i][0]);
-		if (access(cmds_paths[i], F_OK | X_OK) == -1)
+		// ft_printf("Checking %s\n", cmds_argv[i][0]);
+		if (access(cmds_path[i], F_OK | X_OK) == -1)
 		{
-			// ft_printf("pipex: %s: ", cmds[i][0]);
-			perror("Error");
+			ft_putstr_fd(cmds_argv[i][0], STDERR_FILENO);
+			ft_putendl_fd(": command not found", STDERR_FILENO);
 			// free and ends main
+			return (127);
 		}
 		i++;
 	}
 	
-	/* Pipes ================================================================*/
-	int *pipes = (int *)ft_calloc(cmd_count - 1, sizeof(int) * 2);
+	/** Pipes
+	 * ------------------------------------------------------------------------- 
+	 * @brief build pipes between each two commands, equalling to command count 
+	 * minus one.
+	 * 
+	 * @param pipe_count In this case (cmd_count - 1) or (argc - 4)
+	 * 
+	 * @return int pipes[pipe_count * 2]
+	 * Each pairs are the read/write ends of one pipe.
+	 * E.g. pipes[0] pipes[1] for 1st pipe, pipes[2] pipes[3] for second pipe.
+	 */
+	int	*pipes;
+	
+	pipes = (int *)ft_calloc(cmd_count - 1, sizeof(int) * 2);
 	i = 0;
 	while (i < cmd_count - 1) // 0 2 4 6 8
 	{
-		ft_printf("Creating pipe at pipes[%d]\n", i * 2);
+		// ft_printf("Creating pipe at pipes[%d]\n", i * 2);
 		if (pipe(&pipes[i * 2]) == -1)
 		{
 			// free and exit
+			perror("pipex");
 			return (2);
 		}
 		i++;
@@ -136,73 +174,55 @@ int	main(int argc, char **argv)
 		"/usr/local/games:/snap/bin";
 	env[1] = NULL;
 
-	/* First command =========================================================*/
+	/**
+	 * First command
+	 * -------------------------------------------------------------------------
+	 * @brief Execute the first command, which reads input from given infile 
+	 * file descriptor.
+	 * 
+	 * @param infile_fd int
+	 * @param pipes int
+	 * @param cmds_path char**
+	 * @param cmds_argv char***
+	 */
 	pid_t	id_first;
 
 	id_first = fork();
 	if (id_first == -1)
+	{
+		// free all malloc
 		return (1);
+	}
 	if (id_first == 0)
 	{
-		ft_printf("Executing '%s' in %d...\n", cmds[0][0], getpid());
-		ft_printf("pipe write fd: %d, read fd: %d\n", pipes[1], pipes[0]);
-		ft_printf("%d pipe write fd: %d, read fd: %d\n",
-					__LINE__, pipes[1], pipes[0]);
-
-		close(pipes[0]);
 		dup2(infile_fd , STDIN_FILENO);
 		close(infile_fd);
 		dup2(pipes[1], STDOUT_FILENO);
-		close(pipes[1]);
-		if (execve(cmds_paths[0], cmds[0], env) == -1)
+		close_all_pipes(pipes, cmd_count - 1);
+		if (execve(cmds_path[0], cmds_argv[0], env) == -1)
 		{
-			err_msg("First command error\n");
-			perror("Error");
+			perror(cmds_argv[0][0]);
 			return (9);
 		}
-		// free and exit
 	}
 
-	/* Middle commands ====================================================== */
-	/** 
-	 * cmd1 < infile pipes[1] index:0
-	 * cmd2 pipes[0] pipes[3] index:1
-	 * cmd3 pipes[2] pipes[5] index:2 write to pipes[1 + cmd_i * 2]
-	 * cmd4 pipes[4] >outfile index:3 read from pipes[0+ (cmd_i - 1) * 2]
-	 * 
-	 * cmds[0]
-	 * [0] 0 1
-	 * cmds[1] read ((cmd_i - 1) * 2) | write ((cmd_i * 2 + 1)
-	 * [1] 2 3
-	 * cmds[2]
-	 * [2] 4 5
-	 * cmds[3]
-	 * [3] 6 7
-	 * cmds[4]
-	 * [4] 8 9
-	 * cmds[5]
-	 * outfd
-	 * */
+	/* Middle commands 
+	------------------------------------------------------------------------- */
 	int	cmd_i;
 	cmd_i = 1;
 	while (cmd_i < cmd_count - 1)
 	{
 		int	id;
-		int	i;
 		id = fork();
 		if (id == -1)
-			; // free and exit
+			return (1);
 		if (id == 0)
 		{
-			ft_printf("Mid: %s in %d\n", cmds_paths[cmd_i], getpid());
+			// ft_printf("Mid: %s in %d\n", cmds_path[cmd_i], getpid());
 			dup2(pipes[0 + (cmd_i - 1) * 2], STDIN_FILENO);
 			dup2(pipes[1 + cmd_i * 2], STDOUT_FILENO);
-
-			i = 0;
-			while (i < cmd_count - 1)
-				close(pipes[i++]);
-
-			if (execve(cmds_paths[cmd_i], cmds[cmd_i], env) == -1)
+			close_all_pipes(pipes, cmd_count - 1);
+			if (execve(cmds_path[cmd_i], cmds_argv[cmd_i], env) == -1)
 			{
 				perror("Error");
 				return (9);
@@ -211,7 +231,8 @@ int	main(int argc, char **argv)
 		cmd_i++;
 	}
 	
-	/* Last command ========================================================= */
+	/* Last command 
+	------------------------------------------------------------------------- */
 	pid_t	id_last;
 	int		outfile_fd;
 
@@ -220,45 +241,30 @@ int	main(int argc, char **argv)
 		return (2);
 	if (id_last == 0)
 	{
-		ft_printf("%d\n", __LINE__);
-		ft_printf("Executing '%s' in %d\n", cmds[cmd_i][0], getpid());
-
-
 		outfile_fd =  open(argv[argc - 1], O_CREAT | O_WRONLY, 0777);
 		if (outfile_fd == -1)
 		{
 			perror("pipex");
 			return (127);
 		}
-		ft_printf("%d pipe outifle fd: %d, read fd: %d\n",
-					__LINE__, pipes[1 + cmd_i * 2], pipes[0 + (cmd_i - 1) * 2]);
-
-		// close(pipes[1 + cmd_i * 2]);
-		ft_printf("Opened file %s\n", argv[argc - 1]);
-		// pipe[0]: 3, STDIN: 0->terminal
-		// fd pipe[0]: 3 -> pipe-read-end
-		// fd STDIN:   0 -> pipe-read-end
-		// close(pipes[0 + (cmd_i - 1) * 2]);
 		dup2(outfile_fd, STDOUT_FILENO);
 		close(outfile_fd);
 		dup2(pipes[0 + (cmd_i - 1) * 2], STDIN_FILENO);
-		i = 0;
-		while (i < (cmd_count - 1) * 2)
-			close(pipes[i++]);
-		close(pipes[0]);
-		if (execve(cmds_paths[cmd_i], cmds[cmd_i], env) == -1)
+		close_all_pipes(pipes, cmd_count - 1);
+		if (execve(cmds_path[cmd_i], cmds_argv[cmd_i], env) == -1)
 		{
-			err_msg("Last command error\n");
 			// free and exit
-			perror("Error");
+			perror(cmds_argv[cmd_i][0]);
 			return (9);
 		}
 	}
-	i = 0;
-	while (i < (cmd_count - 1) * 2)
-		close(pipes[i++]);
+
+
+	close_all_pipes(pipes, cmd_count - 1);
 	close(infile_fd);
+
 	int	stat_val;
+
 	waitpid(id_last, &stat_val, 0);
 	if (WIFEXITED(stat_val))
 		stat_val = WEXITSTATUS(stat_val);
